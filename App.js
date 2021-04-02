e9bb6a747c8d86c4dec19c0b40d02ddc3f8f4810
Brady Baker <bradybaker82@gmail.com>
2021-04-02 11:04:04 -0500

    Added item array and rendering on DOM

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
-import Header from './components/Header'
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import Header from './components/Header';
+import {v4 as uuid} from 'uuid';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Milk'}
+  ])
+
   return (
     <View style={styles.container}>
-      <Header />
+      <Header title='Shopping List' />
+      <FlatList data={items} renderItem={({item}) => (
+        <Text>{item.text}</Text>
+      )} />
     </View>
   )
 }
